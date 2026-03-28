d7d7f14fe0f6fc110862fdfcaab659a48c32bf67
garvitasingh101 <garvita.singh101@gmail.com>
2026-03-28 19:20:20 -0400

    Add introduction to lab_1a.py

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,7 @@ def main():
     name = "Garvita Singh" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print("Hello everyone!! My name is Garvita Singh, and I am a Junior at Wheeler Magnet from Georgia. I'm interested in pursuing the intersection between computer science and healthcare in the future, so I'm very excited to learn more from this course. I enjoy exploring and teaching software development and I'm super involved in public speaking (any Speech and Debate kids here?). A fun fact about me is that I never cut my hair until I was 13! Looking forward to this course :)")
 
 if __name__ == "__main__":
     main()
